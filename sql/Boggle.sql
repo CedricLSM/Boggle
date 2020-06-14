d7dac8ab6f6e68334196ff409c2d5a7093acc961
Cedric Lee Shao Ming <cedric.lee.2018@sis.smu.edu.sg>
2020-06-14 17:31:59 +0800

    Create Boggle.sql
    Add SQL script for Boggle creation

---
+++ sql/Boggle.sql
@@ -0,0 +1,38 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: car
+--
+CREATE DATABASE IF NOT EXISTS Boggle DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE Boggle;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table Boggle
+--
+
+DROP TABLE IF EXISTS Boggle;
+CREATE TABLE IF NOT EXISTS Boggle (
+  gID int(100) NOT NULL AUTO_INCREMENT,
+  user_score int(100) not null,
+user_answer varchar(600),
+correct_answer varchar(600) not null,
+board varchar(100) not null,
+  PRIMARY KEY (gID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
