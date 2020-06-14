d01153531dd7188bd3093224a89c16cf0ba6366c
Cedric Lee Shao Ming <cedric.lee.2018@sis.smu.edu.sg>
2020-06-14 21:47:17 +0800

    Update Boggle.py
    Add more documentations

--- include/Boggle.py
+++ include/Boggle.py
@@ -10,6 +10,30 @@ CORS(app)
 class Boggle(db.Model):
     """
     A class to handle the logic of the game
+
+    ...
+
+    Parameters
+    ----------
+    user_score : int
+        Score a user gets
+    user_answer : str
+        The answer submitted by a user
+    correct_answer : str, optional
+        All the correct answer contained in the board
+    gID : int
+        ID of a particular game (set to auto-increment in SQL script)
+    board : str
+        The generated 4x4 board for the user to play a game on (default board will be generated)
+
+    Methods
+    -------
+    get_correct_answer()
+
+    make_board()
+
+    json()
+        Returns the Boggle object in JSON form
     """
 
     __tablename__ = 'Boggle'
@@ -30,9 +54,9 @@ class Boggle(db.Model):
         correct_answer : str, optional
             All the correct answer contained in the board
         gID : int, optional
-            ID of a particular game
+            ID of a particular game (set to auto-increment in SQL script)
         board : str, optional
-            The generated 4x4 board for the user to play a game on
+            The generated 4x4 board for the user to play a game on (default board will be generated)
         """
 
         self.gID = gID
@@ -46,23 +70,31 @@ class Boggle(db.Model):
         if self.correct_answer == None:
             self.correct_answer = self.get_correct_answer()
 
-        def get_correct_answer(self): 
-            #TODO: Write function to create Trie and traverse the board using DFS to determine all correct answers
-            return ""
+    def get_correct_answer(self): 
+        #TODO: Write function to create Trie and traverse the board using DFS to determine all correct answers
+        return ""
 
 
-        def make_board(self): 
-            """
-            Returns a 4x4 board.
-            """
-            #TODO: Generate a randomized 4x4 board to be returned
-            board1 = "A,C,E,D,L,U,G,I,E,D,H,T,G,A,F,K"
-            return board1
+    def make_board(self): 
+        """
+        Returns a 4x4 board
+        """
+        #TODO: Generate a randomized 4x4 board to be returned
+        board1 = "A,C,E,D,L,U,G,I,E,D,H,T,G,A,F,K"
+        return board1
 
     def json(self):
+        """
+        Returns the Boggle object in JSON form
+        """
         return {"gID": self.gID, "user_score":self.user_score,"board":self.board,"user_answer": self.user_answer, "correct_answer":self.correct_answer}
 
-
+'''
+App routes to retrieve different Boggle information
+'''
+@app.route("/games")
+def get_all_games():
+    return jsonify({"Boggle": [boggle.json() for boggle in Boggle.query.all()]})
 
 if __name__ == '__main__': #this allows us to run flask app without explicitly using python -m flask run. Can just run python filename.py in terminal
     app.run(host='0.0.0.0',port=7000, debug=True)
